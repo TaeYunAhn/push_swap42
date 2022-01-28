4a4a3a49d01a4e830d0726e9d6539a84da107da7
Taeyun Ahn <tahn@c4r4s3.42seoul.kr>
2022-01-28 19:58:54 +0900

    init

--- sortB.c
+++ sortB.c
@@ -1,9 +1,21 @@
 #include "utils.h"
 
+void	multiPa(aStack, bStack, size)
+{
+	while(size--)
+		pa(aStack, bStack);
+}
+
 void	sortB(t_stack *aStack, t_stack *bStack, int size)
 {
 	int		counts[4];
 	int		pivots[4];
 
+	getPivots(pivots, bStack, size);
+	ft_memset(counts, 0, size);
+	if (isStackSorted(bStack, DESC, sizeof(int) * 4))
+	{
+
+	}
 
 }
