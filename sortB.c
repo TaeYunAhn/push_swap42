1b76d9a3e7137bff5e6df7fdfcad6002b2c050eb
Taeyun Ahn <tahn@c4r4s3.42seoul.kr>
2022-01-28 20:30:48 +0900

    init

--- sortB.c
+++ sortB.c
@@ -1,9 +1,69 @@
 #include "utils.h"
 
-void	multiPa(aStack, bStack, size)
+
+static void		endBRecurse(t_stack *aStack, t_stack *bStack, size)
+{
+	t_node		*bTop;
+	bTop = bStack->list->tail;
+	if (size == 3)
+	{
+		sortBThree(aStack, bStack);
+		return ;
+	}
+	else if (size == 2)
+	{
+		if (bTop->content < bTop->prev->content)
+			sb(bStack);
+		pa(aStack, bStack);
+		pa(aStack, bStack);
+		return ;
+	}
+	else if (size == 1)
+	{
+		pa(aStack, bStack);
+	}
+}
+
+static void		moveStack(t_stack *aStack,
+				t_stack *bStack, int pivots[2], int counts[4])
+{
+	t_node		*bTop;
+
+	bTop = bStack->list->tail;
+	if (bTop->content < pid_t[0])
+	{
+		counts[RB]++;
+		rb(bStack);
+	}
+	else
+	{
+		counts[PUSH]++;
+		pa(aStack, bStack);
+		if (aStack->list->tail->content < pivots[1])
+		{
+			counts[RA]++;
+			ra(aStack);
+		}
+	}
+}
+
+void	multiPa(t_stack aStack, t_stack bStack, int size)
 {
 	while(size--)
 		pa(aStack, bStack);
+}
+
+static void		nextBRecurse(t_stack *aStack, t_stack *bStack, int counts[4])
+{
+	if(isStackSorted(aStack, ASC, count[PUSH] - counts[RA]) == 0)
+		sortA(aStack, bStack, counts[PUSH] - counts[RA]);
+	recoverStack(aStack, bStack, counts[RA], counts[RB]);
+	if (isStackSorted(aStack, ASC, counts[RA]) == 0)
+		sortA(aStack, bStack, counts[RA]);
+	if (isStackSorted(aStack, DESC, counts[RB]) == 0)
+		sortB(aStack, bStack, counts[RB]);
+	else
+		multiPa(aStack, bStack, counts[RB]);
 }
 
 void	sortB(t_stack *aStack, t_stack *bStack, int size)
@@ -15,7 +75,12 @@ void	sortB(t_stack *aStack, t_stack *bStack, int size)
 	ft_memset(counts, 0, size);
 	if (isStackSorted(bStack, DESC, sizeof(int) * 4))
 	{
-
+		multiPa(aStack, bStack, size);
+		return ;
 	}
-
+	if (size <= 3)
+	{
+		endBRecurse(aStack, bStack, size);
+		return ;
+	}
 }
--- sortBThree.c
+++ sortBThree.c
@@ -1,2 +1,77 @@
 #include "utils.h"
 
+static	void	caseOne(t_stack *aStack, t_stack *bStack)
+{
+	t_node		*bTail;
+
+	bTail = bStack->list->tail;
+	if (bTail->content > bTail->prev->prev->content)
+	{
+		pa(aStack, bStack);
+		sb(bStack);
+		pa(aStack, bStack);
+		pa(aStack, bStack);
+		return ;
+	}
+	pa(aStack, bStack);
+	sb(bStack);
+	pa(aStack, bStack);
+	sa(aStack);
+	pa(aStack, bStack);
+}
+
+static	void	caseTwo(t_stack *aStack, t_stack *bStack)
+{
+	pa(aStack, bStack);
+	ra(aStack);
+	sb(bStack);
+	pa(aStack, bStack);
+	pa(aStack, bStack);
+	rra(aStack);
+}
+
+static	void	caseThree(t_stack *aStack, t_stack *bStack)
+{
+	sb(bStack);
+	pa(aStack, bStack);
+	pa(aStack, bStack);
+	pa(aStack, bStack);
+}
+
+static	void	caseFour(t_stack *aStack, t_stack *bStack)
+{
+	pa(aStack, bStack);
+	ra(aStack);
+	pa(aStack, bStack);
+	pa(aStack, bStack);
+	rra(aStack);
+}
+
+void	sortBThree(t_stack *aStack, t_stack *bStack)
+{
+	t_node		*bTail;
+
+	bTail = bStack->list->tail;
+	if (isStackSorted(bStack, DESC, 3))
+	{
+		pa(aStack, bStack);
+		pa(aStack, bStack);
+		pa(aStack, bStack);
+	}
+	else if (bTail->content > bTail->prev->content && \
+		bTail->prev->content < bTail->prev->prev->content)
+		caseOne(aStack, bStack);
+	else if (bTail->content < bTail->prev->content && \
+		bTail->prev->content < bTail->prev->prev->content)
+		caseTwo(aStack, bStack);
+	else if (bTail->content < bTail->prev->content && \
+		bTail->prev->content > bTail->prev->prev->content)
+	{
+		if (bTail->content > bTail->prev->prev->content)
+		{
+			caseThree(aStack, bStack);
+			return ;
+		}
+		caseFour(aStack, bStack);
+	}
+}
